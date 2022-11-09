51b706123d43e1431d7d6965f7f0a9da0a61c96a
javiuz <105435887+javiuz@users.noreply.github.com>
2022-11-09 21:05:04 +0100

    Update f.m

--- CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/NH-Dir B.C. for u and H for p/Non_homog_u_Homog_p/f.m
+++ CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/NH-Dir B.C. for u and H for p/Non_homog_u_Homog_p/f.m
@@ -7,10 +7,10 @@ global alpha
 if k==1     % Primera componente del vector ff (f1)          
 %    ff=alpha*tt;
 %    ff=1;
-    ff=(-1 + 2*xx) *(-1 + yy)*yy;
+    ff=alpha*(-1 + 2*xx)*(-1 + yy)*yy;
 else        % Segunda componente del vector ff (f2)   
 %    ff=0;
-    ff=(-1 + xx)* xx* (-1 + 2* yy);
+    ff=alpha*(-1 + xx)*xx*(-1 + 2* yy);
 end
        
 return
