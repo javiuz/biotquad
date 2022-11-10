6e398ea16d2199b88523f0ca02f8140d900b5be9
javiuz <105435887+javiuz@users.noreply.github.com>
2022-11-10 09:45:07 +0100

    Update sol_exactax_sigma.m

--- CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/NH-Dir B.C. for u and p/Test 1/Non_homog_u_p/sol_exactax_sigma.m
+++ CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/NH-Dir B.C. for u and p/Test 1/Non_homog_u_p/sol_exactax_sigma.m
@@ -5,13 +5,13 @@ global alpha lambda mu
 %       sigma(xx,yy,tt) = [sigma(1,1) sigma(1,2);sigma(2,1) sigma(2,2)]
 
  if row==1 && col==1       % Solución analítica de sigma (1,1) (xx,yy,tt)
-     sol=2-xx;     
+     sol=2*(lambda+mu)-alpha*xx;     
  elseif row==1 && col==2   % Solución analítica de sigma (1,2) (xx,yy,tt)
      sol=0;
  elseif row==2 && col==1   % Solución analítica de sigma (2,1) (xx,yy,tt)
      sol=0;
  else                       % Solución analítica de sigma (2,2) (xx,yy,tt)
-     sol=2-xx;   
+     sol=2*(lambda+mu)-alpha*xx;     
  end
 
 return
