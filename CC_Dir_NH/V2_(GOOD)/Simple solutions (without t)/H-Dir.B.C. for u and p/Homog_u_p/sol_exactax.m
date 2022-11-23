12308775f70205e2c2addbb79211d8580a83ce3d
javiuz <105435887+javiuz@users.noreply.github.com>
2022-11-23 19:26:20 +0100

    Create a new program with Homog. Dir.B.C. for u and p

---
+++ CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/H-Dir.B.C. for u and p/Homog_u_p/sol_exactax.m
@@ -0,0 +1,34 @@
+function sol=sol_exactax(xx,yy,tt,comp)
+
+global perm
+
+%       u(xx,yy) = {x,y}
+
+%         u1(xx) = xx
+%         u2(yy) = yy
+
+%         p(xx,tt) = tt*xx
+
+%         z(tt)= {z1(tt),z2}
+%           z1(tt)=-perm*tt
+%           z2=0
+
+%         gamma = [0 r;-r 0]
+%           r=0
+
+ if comp==1         % Solución analítica de u1(xx)
+     sol=xx*(1-xx)*yy*(1-yy);    
+ elseif comp==2     % Solución analítica de u2(yy)
+     sol=xx*(xx-1)*yy*(1-yy);
+ elseif comp==3     % Solución analítica de p(xx,tt)
+     sol=xx*(1-xx)*yy*(1-yy);
+ elseif comp==4     % Solución analítica de z1(tt)
+     sol=-perm*(-1 + 2*xx)*(-1 + yy)*yy;
+ elseif comp==5     % Solución analítica de z2
+     sol=-perm*(-1 + xx)*xx*(-1 + 2 *yy);
+ else               % Solución analítica de gamma (1st row & 2nd column)
+     sol=(1/2)*(yy + 2*xx*(-1 + yy)*yy - yy^2 + (-1 + xx)*xx*(-1 + 2*yy));
+ end
+
+return
+end
