6f1e377b2057362357bb970ed5cf26cd305ea6f8
javiuz <105435887+javiuz@users.noreply.github.com>
2022-12-21 11:28:36 +0100

    Update f.m

--- CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/13-12-2022/Initial condition--sol.exacta/alpha1=0, alpha2=1/Test 3 -- K successively lower/f.m
+++ CC_Dir_NH/V2_(GOOD)/Simple solutions (without t)/13-12-2022/Initial condition--sol.exacta/alpha1=0, alpha2=1/Test 3 -- K successively lower/f.m
@@ -5,8 +5,8 @@ global alpha lambda mu
 %       ff(xx,yy,t) = {f1,f2}'
     
 if k==1     % Primera componente del vector ff (f1)          
-    ff=alpha;
-%     ff=0;
+%     ff=alpha;
+    ff=0;
 else        % Segunda componente del vector ff (f2)   
     ff=0;
 end
