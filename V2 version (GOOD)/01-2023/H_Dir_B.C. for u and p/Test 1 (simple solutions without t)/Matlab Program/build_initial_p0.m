103839a40a5007752ee8169f0ad858e1632c35f5
javiuz <105435887+javiuz@users.noreply.github.com>
2023-01-17 12:41:45 +0100

    Create a new test (Test 1) for Biot's problem
    We use the integral of simpson in 2D to approximate the initial data p0, but the results are much worse than those that we obtain when considering the exact solution of the pressure in t=0 at the center of the element.

---
+++ V2 version (GOOD)/01-2023/H_Dir_B.C. for u and p/Test 1 (simple solutions without t)/Matlab Program/build_initial_p0.m
@@ -0,0 +1,45 @@
+function p0=build_initial_p0(tt)
+
+global NN 
+
+N=NN;
+
+p0=zeros(N*N,1);
+
+for j=1:N
+    for i=1:N
+        ind=i+(j-1)*N;  
+%         xx=(x(i,j)+x(i+1,j)+x(i+1,j+1)+x(i,j+1))/4;
+%         yy=(y(i,j)+y(i+1,j)+y(i+1,j+1)+y(i,j+1))/4;
+%         E=sin(5*pi*xx)*sin(5*pi*yy)+5;
+%         lambda=E*c_lambda;
+%         mu=E*c_mu;      
+        p0(ind)=int_f_simpson(i,j,tt);
+    end
+end
+return
+end
+
+function intt=int_f_simpson(i,j,tt)
+
+intt=1/36*(g(i,j,0,0,tt)+4*g(i,j,0.5,0,tt)+g(i,j,1,0,tt)+...
+            4*(g(i,j,0,0.5,tt)+4*g(i,j,0.5,0.5,tt)+g(i,j,1,0.5,tt))+...
+            g(i,j,0,1,tt)+4*g(i,j,0.5,1,tt)+g(i,j,1,1,tt));
+            
+return 
+end
+
+function gg=g(i,j,xx,yy,tt)
+
+global x y
+
+t1=area_triangulo(x(i,j+1),y(i,j+1),x(i,j),y(i,j),x(i+1,j),y(i+1,j));
+t2=area_triangulo(x(i,j),y(i,j),x(i+1,j),y(i+1,j),x(i+1,j+1),y(i+1,j+1));
+t4=area_triangulo(x(i+1,j+1),y(i+1,j+1),x(i,j+1),y(i,j+1),x(i,j),y(i,j));
+
+jacob=2*t1+2*(t2-t1)*xx+2*(t4-t1)*yy;
+gg=jacob*sol_exactax(x(i,j)+(x(i+1,j)-x(i,j))*xx+(x(i,j+1)-x(i,j))*yy+(x(i+1,j+1)-x(i,j+1)-x(i+1,j)+x(i,j))*xx*yy,...
+           y(i,j)+(y(i+1,j)-y(i,j))*xx+(y(i,j+1)-y(i,j))*yy+(y(i+1,j+1)-y(i,j+1)-y(i+1,j)+y(i,j))*xx*yy,tt,3);
+
+return
+end
