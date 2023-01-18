eb62af3710b6feebbdca78edc313411153ba7d7f
javiuz <105435887+javiuz@users.noreply.github.com>
2023-01-18 15:29:02 +0100

    Update MS_MF_MFE.m

--- V2 version (GOOD)/01-2023/H_Dir_B.C. for u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
+++ V2 version (GOOD)/01-2023/H_Dir_B.C. for u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
@@ -44,8 +44,10 @@ perm=1;
 t=0;
 % Time step
 delta_t=1e-04;
+% delta_t=1;
 % Final time
 Tf=1e-03;
+% Tf=1;
 
 %  Matrices del sistema de Biot: A11, A12, A21 y A22
     % Asp y App las utilizaremos después
