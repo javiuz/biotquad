4c7ea6779d6dba3ae32afb857509ed52a67f2f8b
javiuz <105435887+javiuz@users.noreply.github.com>
2023-01-31 12:38:35 +0100

    Create Test 1 of Biot's problem with time component in the solutions of u and p
    Non-Homog. Dir.B.C. for u and p.
    lambda=mu=1.

---
+++ V2 version-VERY GOOD/p0 as solution of the stationary system/With time component/Non-Homog. Dir. B.C. for u and p/Test 1 (lambda=mu=1)/Matlab Program/q.m
@@ -0,0 +1,12 @@
+function qq=q(xx,yy,tt)
+
+global c0 perm
+
+%       qq(xx,yy,tt) = q
+    
+qq=-2*perm*(-1 + xx)*xx - 2*perm*(-1 + yy)*yy + ...
+    alpha*exp(tt)*(xx*(2 + 3*xx*yy^4) + (-1 + yy)*cos(1 - yy)*cos((-1 + xx)*(-1 + yy))) + ...
+    c0*exp(tt)*(10 + cos(pi*yy)*sin(pi*xx)) + alpha*exp(tt)*...
+    (2*(-1 + yy) - 3*(-1 + xx)^4*(-1 + yy)^2 - xx*sin(xx)*sin(xx*yy)); 
+return
+end
