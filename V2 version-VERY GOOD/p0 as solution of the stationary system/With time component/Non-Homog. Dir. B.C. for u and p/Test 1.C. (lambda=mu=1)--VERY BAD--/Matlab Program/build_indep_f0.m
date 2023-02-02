3ee37b9be80a616e1c05ac79bd89282434516a4e
javiuz <105435887+javiuz@users.noreply.github.com>
2023-02-02 17:14:54 +0100

    Create 'Test 1.C. (lambda=mu=1)--VERY BAD--
    To obtain the initial data p0=p(x,0), we use the solution of the
    pressure obtained by solving the discrete system for the stationary
    eqs. of the flux problem when t=0:
    
    A22=f'*(t\f);
    q indep. term without delta_t
    gDp=f'*(t\Pgp);
    
    We obtain the rest of the initial data (compatible initial data)
    by computing:
    
    A) The solution for the displacement of the elasticity system when
    t=0, considering implicitly the term alpha*grad p, thus:
    
    sigma_e is considered instead of sigma.
    f=-div(sigma_e) is considered.
    
    B) The rest of the elasticity variables are computed as in the
    elasticity problem (without A_sp^T), once u0 is obtained in the
    previous step A).

---
+++ V2 version-VERY GOOD/p0 as solution of the stationary system/With time component/Non-Homog. Dir. B.C. for u and p/Test 1.C. (lambda=mu=1)--VERY BAD--/Matlab Program/build_indep_f0.m
@@ -0,0 +1,47 @@
+function indep1=build_indep_f0(tt)
+
+global NN 
+
+N=NN;
+
+indep1=zeros(2*N*N,1);
+
+for j=1:N
+    for i=1:N
+        ind2=(i+(j-1)*N)*2;
+        ind1=ind2-1;    
+%         xx=(x(i,j)+x(i+1,j)+x(i+1,j+1)+x(i,j+1))/4;
+%         yy=(y(i,j)+y(i+1,j)+y(i+1,j+1)+y(i,j+1))/4;
+%         E=sin(5*pi*xx)*sin(5*pi*yy)+5;
+%         lambda=E*c_lambda;
+%         mu=E*c_mu;      
+        indep1(ind1)=int_f_simpson(i,j,1,tt);
+        indep1(ind2)=int_f_simpson(i,j,2,tt);
+    end
+end
+return
+end
+
+function intt=int_f_simpson(i,j,k,tt)
+
+intt=1/36*(g(i,j,0,0,k,tt)+4*g(i,j,0.5,0,k,tt)+g(i,j,1,0,k,tt)+...
+            4*(g(i,j,0,0.5,k,tt)+4*g(i,j,0.5,0.5,k,tt)+g(i,j,1,0.5,k,tt))+...
+            g(i,j,0,1,k,tt)+4*g(i,j,0.5,1,k,tt)+g(i,j,1,1,k,tt));
+            
+return 
+end
+
+function gg=g(i,j,xx,yy,k,tt)
+
+global x y 
+
+t1=area_triangulo(x(i,j+1),y(i,j+1),x(i,j),y(i,j),x(i+1,j),y(i+1,j));
+t2=area_triangulo(x(i,j),y(i,j),x(i+1,j),y(i+1,j),x(i+1,j+1),y(i+1,j+1));
+t4=area_triangulo(x(i+1,j+1),y(i+1,j+1),x(i,j+1),y(i,j+1),x(i,j),y(i,j));
+
+jacob=2*t1+2*(t2-t1)*xx+2*(t4-t1)*yy;
+gg=jacob*f0(x(i,j)+(x(i+1,j)-x(i,j))*xx+(x(i,j+1)-x(i,j))*yy+(x(i+1,j+1)-x(i,j+1)-x(i+1,j)+x(i,j))*xx*yy,...
+           y(i,j)+(y(i+1,j)-y(i,j))*xx+(y(i,j+1)-y(i,j))*yy+(y(i+1,j+1)-y(i,j+1)-y(i+1,j)+y(i,j))*xx*yy,k,tt);
+
+return
+end
---
+++ V2 version-VERY GOOD/p0 as solution of the stationary system/With time component/Non-Homog. Dir. B.C. for u and p/Test 1.C. (lambda=mu=1)--VERY BAD--/Matlab Program/sol_exacta_p.m
@@ -0,0 +1,13 @@
+function sol=sol_exacta_p(i,j,tt)
+
+global x  y
+
+xx=(x(i,j)+x(i+1,j)+x(i+1,j+1)+x(i,j+1))/4;
+yy=(y(i,j)+y(i+1,j)+y(i+1,j+1)+y(i,j+1))/4;
+
+% p(xx,yy,tt)
+
+sol=exp(tt)*(10 + cos(pi*yy)*sin(pi*xx));
+
+return
+end
