4bb9a1cb42d43a62b3d9653ffdc9c158e7ea81f8
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-05 20:01:24 +0200

    Update compute_fluxes.m

--- V2 version -- 30-04-2023/Matrix Azz -- V2 (same signs as Ass)/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/compute_fluxes.m
+++ V2 version -- 30-04-2023/Matrix Azz -- V2 (same signs as Ass)/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/compute_fluxes.m
@@ -40,8 +40,8 @@ Jer1=abs(x2*y1 - x4*y1 - x1*y2 + x4*y2 + x1*y4 - x2*y4);
 SW_denom_zz=Jer1*4;
 
 f=zeros(vdim,1);
-f(1,1)=-(1/2);
-f(2,1)=-(1/2);
+f(1,1)=1/2;
+f(2,1)=1/2;
 
 t=zeros(vdim,vdim);
 t(1,1)=Kinv(1,1)*(x1 - x4)^2 + (2*Kinv(1,2)*(x1 - x4) + Kinv(2,2)*(y1 - y4))*(y1 - y4);
@@ -55,9 +55,8 @@ t=t/SW_denom_zz;
     % frontera Sur
         Pgp_L1=int_dir_simpson(i,j,i+1,j,tt,3);
     % frontera Oeste
-        Pgp_L4=int_dir_simpson(i,j,i,j+1,tt,3);
-%         Pgp=(1/2)*[Pgp_L1;Pgp_L4];
-        Pgp=-(1/2)*[Pgp_L1;Pgp_L4];
+        Pgp_L4=int_dir_simpson(i,j,i,j+1,tt,3); 
+        Pgp=(1/2)*[Pgp_L1;Pgp_L4];
 
 pp(1,1)=p(i,j);
 zz=t\(Pgp-f*pp);
@@ -77,9 +76,9 @@ pp=zeros(pdim,1);
 f=zeros(vdim,2);
 % f(1,1)=0;
 f(2,1)=-(1/2);
-f(3,1)=-(1/2);
-f(1,2)=-(1/2);
-f(2,2)=-(1/2);
+f(3,1)=1/2;
+f(1,2)=1/2;
+f(2,2)=1/2;
 % f(3,2)=0;
 
 t=zeros(vdim,vdim);
@@ -111,7 +110,7 @@ for i=2:N
     t(1,2)=t(2,1);
     t(2,2)=(Kinv(1,1)*(x1 - x2)^2 + (2*Kinv(1,2)*(x1 - x2) + Kinv(2,2)*(y1 - y2))*(y1 - y2))/S_denom_E1_zz +...
            (Kinv(1,1)*(x2 - x5)^2 + (2*Kinv(1,2)*(x2 - x5) + Kinv(2,2)*(y2 - y5))*(y2 - y5))/S_denom_E2_zz;
-    t(3,2)=-(Kinv(1,1)*(x1 - x2)*(x2 - x3) + Kinv(2,2)*(y1 - y2)*(y2 - y3) + Kinv(1,2)*(x3*(-y1 + y2) + x1*(y2 - y3) + x2*(y1 - 2*y2 + y3)))/S_denom_E1_zz;
+    t(3,2)=(Kinv(1,1)*(x1 - x2)*(x2 - x3) + Kinv(2,2)*(y1 - y2)*(y2 - y3) + Kinv(1,2)*(x3*(-y1 + y2) + x1*(y2 - y3) + x2*(y1 - 2*y2 + y3)))/S_denom_E1_zz;
 %     t(1,3)=0;
     t(2,3)=t(3,2);
     t(3,3)=(Kinv(1,1)*(x2 - x3)^2 + (2*Kinv(1,2)*(x2 - x3) + Kinv(2,2)*(y2 - y3))*(y2 - y3))/S_denom_E1_zz;
@@ -121,8 +120,7 @@ for i=2:N
             Pgp_L2=int_dir_simpson(i,j,i+1,j,tt,3); 
         % frontera Sur 2
             Pgp_L1=int_dir_simpson(i-1,j,i,j,tt,3); 
-%         Pgp=(1/2)*[Pgp_L2;0;Pgp_L1];
-        Pgp=-(1/2)*[Pgp_L2;0;Pgp_L1];
+        Pgp=(1/2)*[Pgp_L2;0;Pgp_L1];
     
     pp(1,1)=p(i-1,j);
     pp(2,1)=p(i,j);
@@ -156,11 +154,11 @@ SE_denom_zz=Jer2*4;
 
 f=zeros(vdim,1);
 f(1,1)=-(1/2);
-f(2,1)=-(1/2);
+f(2,1)=1/2;
 
 t=zeros(vdim,vdim);
 t(1,1)=Kinv(1,1)*(x1 - x2)^2 + (2*Kinv(1,2)*(x1 - x2) + Kinv(2,2)*(y1 - y2))*(y1 - y2);
-t(2,1)=-(Kinv(1,1)*(x1 - x2)*(x2 - x3)) + Kinv(2,2)*(y1 - y2)*(y2 - y3) + ...
+t(2,1)=(Kinv(1,1)*(x1 - x2)*(x2 - x3)) + Kinv(2,2)*(y1 - y2)*(y2 - y3) + ...
          Kinv(1,2)*(x3*(-y1 + y2) + x1*(y2 - y3) + x2*(y1 - 2*y2 + y3));
 t(1,2)=t(2,1);
 t(2,2)=Kinv(1,1)*(x2 - x3)^2 + (2*Kinv(1,2)*(x2 - x3) + Kinv(2,2)*(y2 - y3))*(y2 - y3);
@@ -171,8 +169,7 @@ t=t/SE_denom_zz;
         Pgp_L7=int_dir_simpson(i,j,i,j+1,tt,3);
     % frontera Sur
         Pgp_L3=int_dir_simpson(i-1,j,i,j,tt,3);
-%     Pgp=(1/2)*[-Pgp_L7;Pgp_L3];
-    Pgp=-(1/2)*[Pgp_L7;Pgp_L3];
+    Pgp=(1/2)*[-Pgp_L7;Pgp_L3];
 
 pp(1,1)=p(i-1,j);
 zz=t\(Pgp-f*pp);
@@ -210,16 +207,16 @@ for j=2:N
     W_denom_E2_zz=4*JE2r1;
     
     f=zeros(vdim,2);
-    f(1,1)=-(1/2);
+    f(1,1)=1/2;
     f(2,1)=-(1/2);
 %     f(3,1)=0;
 %     f(1,2)=0;
-    f(2,2)=-(1/2);
-    f(3,2)=-(1/2);
+    f(2,2)=1/2;
+    f(3,2)=1/2;
     
     t=zeros(vdim,vdim);
     t(1,1)=(Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/W_denom_E1_zz;
-    t(2,1)=(Kinv(1,1)*(x1 - x4)*(x3 - x4) + Kinv(2,2)*(y1 - y4)*(y3 - y4) - ...
+    t(2,1)=(Kinv(1,1)*(x1 - x4)*(-x3 + x4) + Kinv(2,2)*(y1 - y4)*(-y3 + y4) + ...
             Kinv(1,2)*(x4*(y1 + y3 - 2*y4) + x3*(-y1 + y4) + x1*(-y3 + y4)))/W_denom_E1_zz;
 %     t(3,1)=0;
     t(1,2)=t(2,1);
@@ -236,8 +233,7 @@ for j=2:N
             Pgp_L4=int_dir_simpson(i,j,i,j-1,tt,3);
         % frontera Oeste 2
             Pgp_L11=int_dir_simpson(i,j,i,j+1,tt,3);
-%         Pgp=(1/2)*[Pgp_L4;0;Pgp_L11];
-        Pgp=-(1/2)*[Pgp_L4;0;Pgp_L11];
+        Pgp=(1/2)*[Pgp_L4;0;Pgp_L11];
         
     pp(1,1)=p(i,j-1);
     pp(2,1)=p(i,j);
@@ -263,18 +259,18 @@ for j=2:N
 %     f(2,1)=0;
 %     f(3,1)=0;
     f(4,1)=-(1/2);
-    f(1,2)=-(1/2);
+    f(1,2)=1/2;
     f(2,2)=-(1/2);
 %     f(3,2)=0;
 %     f(4,2)=0;
 %     f(1,3)=0;
 %     f(2,3)=0;
     f(3,3)=-(1/2);
-    f(4,3)=-(1/2);
+    f(4,3)=1/2;
 %     f(1,4)=0;
-    f(2,4)=-(1/2);
-    f(3,4)=-(1/2);
-%     f(4,4)=0;   
+    f(2,4)=1/2;
+    f(3,4)=1/2;
+%     f(4,4)=0;
     
     for i=2:N
         
@@ -310,8 +306,8 @@ for j=2:N
 
     t(1,1)=(Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/I_denom_E1_zz +...
            (Kinv(1,1)*(x3 - x6)^2 + (2*Kinv(1,2)*(x3 - x6) + Kinv(2,2)*(y3 - y6))*(y3 - y6))/I_denom_E2_zz;
-    t(2,1)=-(Kinv(1,1)*(x2 - x3)*(x3 - x6) + Kinv(2,2)*(y2 - y3)*(y3 - y6) + ...
-             Kinv(1,2)*(x6*(-y2 + y3) + x2*(y3 - y6) + x3*(y2 - 2*y3 + y6)))/I_denom_E2_zz;
+    t(2,1)=(Kinv(1,1)*(x2 - x3)*(x3 - x6) + Kinv(2,2)*(y2 - y3)*(y3 - y6) + ...
+            Kinv(1,2)*(x6*(-y2 + y3) + x2*(y3 - y6) + x3*(y2 - 2*y3 + y6)))/I_denom_E2_zz;
 %     t(3,1)=0;
     t(4,1)=-(Kinv(1,1)*(x2 - x3)*(x3 - x4) + Kinv(2,2)*(y2 - y3)*(y3 - y4) + ...
              Kinv(1,2)*(x4*(-y2 + y3) + x2*(y3 - y4) + x3*(y2 - 2*y3 + y4)))/I_denom_E1_zz;
@@ -325,8 +321,8 @@ for j=2:N
     t(2,3)=t(3,2);
     t(3,3)=(Kinv(1,1)*(x3 - x6)^2 + (2*Kinv(1,2)*(x3 - x6) + Kinv(2,2)*(y3 - y6))*(y3 - y6))/I_denom_E3_zz +...
            (Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/I_denom_E4_zz;
-    t(4,3)=(Kinv(1,1)*(x3 - x4)*(x3 - x8) + Kinv(2,2)*(y3 - y4)*(y3 - y8) - ...
-            Kinv(1,2)*(x8*(y3 - y4) + x4*(y3 - y8) + x3*(-2*y3 + y4 + y8)))/I_denom_E4_zz;
+    t(4,3)=-(Kinv(1,1)*(x3 - x4)*(x3 - x8) + Kinv(2,2)*(y3 - y4)*(y3 - y8) - ...
+             Kinv(1,2)*(x8*(y3 - y4) + x4*(y3 - y8) + x3*(-2*y3 + y4 + y8)))/I_denom_E4_zz;
     t(1,4)=t(4,1);
 %     t(2,4)=0;
     t(3,4)=t(4,3);
@@ -381,7 +377,7 @@ for j=2:N
     f(3,1)=-(1/2);
 %     f(1,2)=0;
     f(2,2)=-(1/2);
-    f(3,2)=-(1/2);
+    f(3,2)=1/2;
 
     t=zeros(vdim,vdim);
     t(1,1)=(Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/E_denom_E1_zz;
@@ -390,8 +386,8 @@ for j=2:N
              Kinv(1,2)*(x4*(-y2 + y3) + x2*(y3 - y4) + x3*(y2 - 2*y3 + y4)))/E_denom_E1_zz;
 %     t(1,2)=0;
     t(2,2)=(Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/E_denom_E2_zz;
-    t(3,2)=(Kinv(1,1)*(x3 - x4)*(x3 - x5) + Kinv(2,2)*(y3 - y4)*(y3 - y5) - ...
-            Kinv(1,2)*(x5*(y3 - y4) + x4*(y3 - y5) + x3*(-2*y3 + y4 + y5)))/E_denom_E2_zz;
+    t(3,2)=-(Kinv(1,1)*(x3 - x4)*(x3 - x5) + Kinv(2,2)*(y3 - y4)*(y3 - y5) - ...
+             Kinv(1,2)*(x5*(y3 - y4) + x4*(y3 - y5) + x3*(-2*y3 + y4 + y5)))/E_denom_E2_zz;
     t(1,3)=t(3,1);
     t(2,3)=t(3,2);
     t(3,3)=(Kinv(1,1)*(x2 - x3)^2 + (2*Kinv(1,2)*(x2 - x3) + Kinv(2,2)*(y2 - y3))*(y2 - y3))/E_denom_E1_zz +...
@@ -402,7 +398,6 @@ for j=2:N
             Pgp_L7=int_dir_simpson(i,j,i,j-1,tt,3);
         % frontera Este 2
             Pgp_L14=int_dir_simpson(i,j,i,j+1,tt,3);
-%         Pgp=(1/2)*[-Pgp_L7;-Pgp_L14;0];
         Pgp=-(1/2)*[Pgp_L7;Pgp_L14;0];
 
     pp(1,1)=p(i-1,j-1);
@@ -438,12 +433,12 @@ Jer4=abs(x3*y1 - x4*y1 - x1*y3 + x4*y3 + x1*y4 - x3*y4);
 NW_denom_zz=4*Jer4;
 
 f=zeros(vdim,1);
-f(1,1)=-(1/2);
+f(1,1)=1/2;
 f(2,1)=-(1/2);
 
 t=zeros(vdim,vdim);
 t(1,1)=Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4);
-t(2,1)=Kinv(1,1)*(x1 - x4)*(x3 - x4) + Kinv(2,2)*(y1 - y4)*(y3 - y4) - ...
+t(2,1)=Kinv(1,1)*(x1 - x4)*(-x3 + x4) + Kinv(2,2)*(y1 - y4)*(-y3 + y4) + ...
        Kinv(1,2)*(x4*(y1 + y3 - 2*y4) + x3*(-y1 + y4) + x1*(-y3 + y4));
 t(1,2)=t(2,1);
 t(2,2)=Kinv(1,1)*(x1 - x4)^2 + (2*Kinv(1,2)*(x1 - x4) + Kinv(2,2)*(y1 - y4))*(y1 - y4);
@@ -454,8 +449,7 @@ t=t/NW_denom_zz;
         Pgp_L18=int_dir_simpson(i,j,i,j-1,tt,3);
     % frontera Norte
         Pgp_L22=int_dir_simpson(i,j,i+1,j,tt,3);
-%     Pgp=(1/2)*[Pgp_L18;-Pgp_L22];
-    Pgp=-(1/2)*[Pgp_L18;Pgp_L22];
+    Pgp=(1/2)*[Pgp_L18;-Pgp_L22];
 
 pp(1,1)=p(i,j-1);
 zz=t\(Pgp-f*pp);
@@ -476,7 +470,7 @@ f=zeros(vdim,2);
 f(1,1)=-(1/2);
 % f(2,1)=0;
 f(3,1)=-(1/2);
-f(1,2)=-(1/2);
+f(1,2)=1/2;
 f(2,2)=-(1/2);
 % f(3,2)=0;
 
@@ -506,8 +500,8 @@ for i=2:N
 
     t(1,1)=(Kinv(1,1)*(x3 - x4)^2 + (2*Kinv(1,2)*(x3 - x4) + Kinv(2,2)*(y3 - y4))*(y3 - y4))/N_denom_E1_zz +...
            (Kinv(1,1)*(x3 - x6)^2 + (2*Kinv(1,2)*(x3 - x6) + Kinv(2,2)*(y3 - y6))*(y3 - y6))/N_denom_E2_zz;
-    t(2,1)=-(Kinv(1,1)*(x2 - x3)*(x3 - x6) + Kinv(2,2)*(y2 - y3)*(y3 - y6) + ...
-             Kinv(1,2)*(x6*(-y2 + y3) + x2*(y3 - y6) + x3*(y2 - 2*y3 + y6)))/N_denom_E2_zz;
+    t(2,1)=(Kinv(1,1)*(x2 - x3)*(x3 - x6) + Kinv(2,2)*(y2 - y3)*(y3 - y6) + ...
+            Kinv(1,2)*(x6*(-y2 + y3) + x2*(y3 - y6) + x3*(y2 - 2*y3 + y6)))/N_denom_E2_zz;
     t(3,1)=-(Kinv(1,1)*(x2 - x3)*(x3 - x4) + Kinv(2,2)*(y2 - y3)*(y3 - y4) + ...
              Kinv(1,2)*(x4*(-y2 + y3) + x2*(y3 - y4) + x3*(y2 - 2*y3 + y4)))/N_denom_E1_zz;
     t(1,2)=t(2,1);
@@ -522,7 +516,6 @@ for i=2:N
             Pgp_L23=int_dir_simpson(i,j,i+1,j,tt,3);
         % frontera Norte 2
             Pgp_L22=int_dir_simpson(i-1,j,i,j,tt,3);
-%         Pgp=(1/2)*[0;-Pgp_L23;-Pgp_L22];
         Pgp=-(1/2)*[0;Pgp_L23;Pgp_L22];
   
     pp(1,1)=p(i-1,j-1);
@@ -574,7 +567,6 @@ t=t/NE_denom_zz;
         Pgp_L21=int_dir_simpson(i,j,i,j-1,tt,3);
     % frontera Norte
         Pgp_L24=int_dir_simpson(i-1,j,i,j,tt,3);
-%     Pgp=(1/2)*[-Pgp_L21;-Pgp_L24];
     Pgp=-(1/2)*[Pgp_L21;Pgp_L24];
 
 pp(1,1)=p(i-1,j-1);
