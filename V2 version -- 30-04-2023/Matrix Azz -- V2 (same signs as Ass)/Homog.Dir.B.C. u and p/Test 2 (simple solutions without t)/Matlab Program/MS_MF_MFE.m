bcf0ddf9f29f07da867ef520a34dfd3332c5992b
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-09 00:32:53 +0200

    Update MS_MF_MFE.m

--- V2 version -- 30-04-2023/Matrix Azz -- V2 (same signs as Ass)/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
+++ V2 version -- 30-04-2023/Matrix Azz -- V2 (same signs as Ass)/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
@@ -26,6 +26,11 @@ mu=1;
 c0=1e-05;
 
 % Hydraulic conductivity: K=[perm 0;0 perm];
+% perms=[1,1e-03,1e-06,1e-09,1e-12];
+% for cp=1:5
+%     perm=perms(cp);
+%     perm
+tic
 perm=1;
 % perm=1e-03;
 % perm=1e-06;
@@ -53,38 +58,38 @@ Biot_matrix=[A11 A12;A21 A22];
 %% Terms involving time
 
 % Initial solution of the variables at t=0 
-% u=zeros(2*N*N,1);
+u=zeros(2*N*N,1);
 p=zeros(N*N,1);
 
 for j=1:N
     for i=1:N
-%         ind2u=(i+(j-1)*N)*2;
-%         ind1u=ind2u-1;
-%         ind1p=ind2u/2;
-        ind1p=i+(j-1)*N;
+        ind2u=(i+(j-1)*N)*2;
+        ind1u=ind2u-1;
+        ind1p=ind2u/2;
+%         ind1p=i+(j-1)*N;
         
         xx=(x(i,j)+x(i+1,j)+x(i+1,j+1)+x(i,j+1))/4;
         yy=(y(i,j)+y(i+1,j)+y(i+1,j+1)+y(i,j+1))/4;
         
-%         u(ind1u)=sol_exactax(xx,yy,t,1);
-%         u(ind2u)=sol_exactax(xx,yy,t,2);
+        u(ind1u)=sol_exactax(xx,yy,t,1);
+        u(ind2u)=sol_exactax(xx,yy,t,2);
         p(ind1p)=sol_exactax(xx,yy,t,3);
     end
 end
 
 %% solution of the elasticity system (sigma0):
 
-% Source term of the MSMFE discretization at t=0
-f_indep=build_indep_f(t);        % Source term f
-
-% For non-homogeneous Dir. B.C.
-[gDu,~]=dir_bc_Pg(delta_t,t);   
-    
-    % Right-hand side of the Elasticity system
-indep_elas=f_indep + gDu -A12*p; 
-
-% We solve for the displacement in the elasticity system
-u=A11\indep_elas;
+% % Source term of the MSMFE discretization at t=0
+% f_indep=build_indep_f(t);        % Source term f
+% 
+% % For non-homogeneous Dir. B.C.
+% [gDu,~]=dir_bc_Pg(delta_t,t);   
+%     
+%     % Right-hand side of the Elasticity system
+% indep_elas=f_indep + gDu -A12*p; 
+% 
+% % We solve for the displacement in the elasticity system
+% u=A11\indep_elas;
 
 %% Now we compute the rest of the elasticity variables at t=0:
         % rotation 
@@ -180,6 +185,9 @@ errorp_3_inf
 errorg_L2_inf
 error_sigma_sigmah_inf
 error_z_zh_inf
+
+% toc
+% end
 
 % % We display the L2 norms of the errors of the variables at final time step
 % t
@@ -383,5 +391,6 @@ error_z_zh_inf
 % set(gca, 'XTick', []);
 % colorbar
 % title('Stress (2nd row):computed sol')
+
 toc
 end
