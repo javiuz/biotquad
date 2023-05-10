e52e4118a3505f5902f41ce680ea1d3f9bf4a1a2
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-10 20:44:34 +0200

    Update MS_MF_MFE.m

--- V2 version -- 10-05-2023/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
+++ V2 version -- 10-05-2023/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
@@ -6,7 +6,7 @@ NN=N;       % Dimensión del problema discreto.
 
 % Generación de la malla: ¡OJO! Para la malla nº 3 tenemos que introducir 
 % el valor del nº de refinamientos de manera manual.
-mesh=0;                 
+mesh=3;                 
 [x,y]=init_mesh(mesh);  % coordenadas de los vértices de la malla.
 tic
 
@@ -59,43 +59,11 @@ Asp=AspT';
 Biot_matrix=[A11 A12;A21 A22];
 % dBM=decomposition(Biot_matrix);
 
-%% Terms involving time
+%% Initialize the pressure
 
-% Initial solution of the pressure at t=0 
-p0 = @(x,y) (1 - x).*x.*(1 - y).*y;
-p=zeros(N*N,1);
+% p=init_p0_mesh0; % On the cartesian mesh
+p=init_p0_mesh3; % On the O(h^2)-mesh
 
-for j=1:N
-    for i=1:N
-        ind1p=i+(j-1)*N;
-        
-        % Coordenadas de los vértices de la celda
-        x1=x(i,j);
-        y1=y(i,j);
-        x2=x(i+1,j);
-        y2=y(i+1,j);
-        x3=x(i+1,j+1);
-        y3=y(i+1,j+1);
-        x4=x(i,j+1);
-        y4=y(i,j+1);
-        
-        xcord=[x1,x2,x3,x4];
-        ycord=[y1,y2,y3,y4];
-        
-        % Límites de la celda rectangular
-        xmin=min(xcord);
-        ymin=min(ycord);
-        xmax=max(xcord);
-        ymax=max(ycord);
-        
-        % Área total de la celda rectangular
-        Ac=area_cuadrilatero(x1,y1,x2,y2,x3,y3,x4,y4);
-        
-        % Aproximación de la presión como la media de la presión en toda 
-        % la celda
-        p(ind1p)=(1/Ac)*integral2(p0, xmin, xmax, ymin, ymax);
-    end
-end
 
 %% solution of the elasticity system (sigma0):
 
