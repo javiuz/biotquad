fd50741edbf43d387e49b71f12a0f81b6dec85f2
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-12 20:16:31 +0200

    Update MS_MF_MFE.m

--- V2 version -- 10-05-2023/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
+++ V2 version -- 10-05-2023/Homog.Dir.B.C. u and p/Test 2 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
@@ -62,7 +62,8 @@ Biot_matrix=[A11 A12;A21 A22];
 %% Initialize the pressure
 
 % p=init_p0_mesh0; % On the cartesian mesh
-p=init_p0_mesh3; % On the O(h^2)-mesh
+% p=init_p0_mesh3; % On the O(h^2)-mesh
+p=init_p0_mesh3_v2; % On the O(h^2)-mesh
 
 
 %% solution of the elasticity system (sigma0):
