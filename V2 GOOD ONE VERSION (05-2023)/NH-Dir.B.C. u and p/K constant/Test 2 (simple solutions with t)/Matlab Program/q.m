3a41f1907e10c951174f1b20b48041af99a33c8f
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-16 16:24:56 +0200

    Update q.m

--- V2 GOOD ONE VERSION (05-2023)/NH-Dir.B.C. u and p/K constant/Test 2 (simple solutions with t)/Matlab Program/q.m
+++ V2 GOOD ONE VERSION (05-2023)/NH-Dir.B.C. u and p/K constant/Test 2 (simple solutions with t)/Matlab Program/q.m
@@ -4,7 +4,8 @@ global c0 perm alpha
 
 %       qq(xx,yy,tt) = q
     
-qq=c0*(-1 + xx)*xx*(-1 + yy)*yy - 2*perm*tt*(-xx + xx^2 + (-1 + yy)*yy) + ...
-    alpha*(-((-1 + yy)*yy) + xx^2*(-1 + 2*yy) + xx*(1 - 4*yy + 2*yy^2)); 
+qq= exp(tt)*(2*alpha*(-1 + yy) - 3*alpha*(-1 + xx)^4*(-1 + yy)^2 + alpha*xx*(2 + 3*xx*yy^4) + ...
+    alpha*(-1 + yy)*cos(1 - yy)*cos((-1 + xx)*(-1 + yy)) + 2*perm*pi^2*cos(pi*yy)*sin(pi*xx) + ...
+    c0*(10 + cos(pi*yy)*sin(pi*xx)) - alpha*xx*sin(xx)*sin(xx*yy)); 
 return
 end
