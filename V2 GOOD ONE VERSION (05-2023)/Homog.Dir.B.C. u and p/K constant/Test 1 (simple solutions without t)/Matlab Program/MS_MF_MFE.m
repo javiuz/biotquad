5b58e407e5ca790bf5da5ec31b6da8b09cfff8ce
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-17 17:27:15 +0200

    Update MS_MF_MFE.m

--- V2 GOOD ONE VERSION (05-2023)/Homog.Dir.B.C. u and p/K constant/Test 1 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
+++ V2 GOOD ONE VERSION (05-2023)/Homog.Dir.B.C. u and p/K constant/Test 1 (simple solutions without t)/Matlab Program/MS_MF_MFE.m
@@ -29,16 +29,16 @@ c0=1e-05;
 
 % Hydraulic conductivity: is inside the function 'kinv.m'
 % K=perm*[1 0;0 1]; with perm=1, 1e-03, 1e-06, 1e-09, 1e-12
-% perm=1;
+perm=1;
 % perm=1e-03;
 % perm=1e-06;
 % perm=1e-09;
-perm=1e-12;
+% perm=1e-12;
 
 % perms=[1,1e-03,1e-06,1e-09,1e-12];
 % for kp=1:5
 % perm=perms(kp)
-% ... end (line 190)
+% ... end (line 177)
 
 % initial time
 t=0;
@@ -173,6 +173,8 @@ errorp_3_inf
 errorg_L2_inf
 error_sigma_sigmah_inf
 error_z_zh_inf
+
+% end
 
 % % We display the L2 norms of the errors of the variables at final time step
 % t
