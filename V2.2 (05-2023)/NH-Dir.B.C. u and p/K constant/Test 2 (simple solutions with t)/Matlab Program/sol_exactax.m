8813499baf4a1413ac8e01388707578ffe899ba5
javiuz <105435887+javiuz@users.noreply.github.com>
2023-05-23 12:55:52 +0200

    Create the folder 'V2.2 (05-2023)' in the following directory (see description)
    \GitHub\biotquad

---
+++ V2.2 (05-2023)/NH-Dir.B.C. u and p/K constant/Test 2 (simple solutions with t)/Matlab Program/sol_exactax.m
@@ -0,0 +1,30 @@
+function sol=sol_exactax(xx,yy,tt,comp)
+
+global perm
+
+%       u(xx,yy,tt) = {u1(xx,yy,tt),u2(xx,yy,tt)}
+
+%         p(xx,yy,tt)
+
+%         z(xx,yy,tt)= {z1(xx,yy,tt),z2xx,yy,tt)}
+
+%         gamma = [0 r;-r 0]
+%               = r
+
+ if comp==1         % Solución analítica de u1(xx,yy,tt)
+     sol=exp(tt)*(xx^2 + xx^3*yy^4 + cos(1 - yy)*sin((1 - xx)*(1 - yy)));
+ elseif comp==2     % Solución analítica de u2(xx,yy,tt)
+     sol=exp(tt)*((1 - yy)^2 + (1 - xx)^4*(1 - yy)^3 + cos(xx*yy)*sin(xx));
+ elseif comp==3     % Solución analítica de p(xx,yy,tt)
+     sol=exp(tt)*(10 + cos(pi*yy)*sin(pi*xx));
+ elseif comp==4     % Solución analítica de z1(xx,yy,tt)
+     sol=-(exp(tt)*perm*pi*cos(pi*xx)*cos(pi*yy));
+ elseif comp==5     % Solución analítica de z2(xx,yy,tt)
+     sol=exp(tt)*perm*pi*sin(pi*xx)*sin(pi*yy);
+ else               % Solución analítica de gamma (1st row & 2nd column)
+     sol=(exp(tt)*(4*(-1 + xx)^3*(-1 + yy)^3 + 4*xx^3*yy^3 + (-1 + xx)*cos(1 - yy)*cos((-1 + xx)*(-1 + yy)) -...
+          cos(xx)*cos(xx*yy) + sin(1 - yy)*sin((-1 + xx)*(-1 + yy)) + yy*sin(xx)*sin(xx*yy)))/2.;
+ end
+
+return
+end
